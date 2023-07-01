563ae37f8004f508a579e5e58ec720a21ed9cd6b
vboxuser1986 <>
2023-07-01 18:00:13 +0200

    hw1 youtube analitics

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,17 @@
-
+import os
+from googleapiclient.discovery import build
+import json
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=3))
